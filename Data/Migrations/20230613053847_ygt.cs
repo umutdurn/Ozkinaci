bd1cc969f5be92bc9d2d059102c6ded9188d2c5c
yigit cetiner <cetineryigit@hotmail.com>
2023-06-13 09:14:59 +0300

    re

---
+++ Data/Migrations/20230613053847_ygt.cs
@@ -0,0 +1,263 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ygt : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Admin",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Mail = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Admin", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CarBrands",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Order = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CarBrands", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Order = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Expertiz",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    OnTampon = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    OnSagCamurluk = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    OnSolCamurluk = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Kaput = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    OnSagKapi = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    OnSolKapi = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ArkaSagKapi = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ArkaSolKapi = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Tavan = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ArkaSolCamurluk = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ArkaSagCamurluk = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    BagajKapagi = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ArkaTampon = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Expertiz", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CarModels",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CategoryId = table.Column<int>(type: "int", nullable: false),
+                    CarBrandId = table.Column<int>(type: "int", nullable: false),
+                    Detail = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Order = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CarModels", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CarModels_CarBrands_CarBrandId",
+                        column: x => x.CarBrandId,
+                        principalTable: "CarBrands",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CarModels_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Equipment",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Order = table.Column<int>(type: "int", nullable: false),
+                    CarModelId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Equipment", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Equipment_CarModels_CarModelId",
+                        column: x => x.CarModelId,
+                        principalTable: "CarModels",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Advert",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Showcase = table.Column<bool>(type: "bit", nullable: false),
+                    CarModelId = table.Column<int>(type: "int", nullable: false),
+                    EquipmentId = table.Column<int>(type: "int", nullable: true),
+                    Gear = table.Column<int>(type: "int", nullable: false),
+                    Fuel = table.Column<int>(type: "int", nullable: false),
+                    Year = table.Column<int>(type: "int", nullable: false),
+                    Km = table.Column<float>(type: "real", nullable: false),
+                    MaxSpeed = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    To0100 = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Tork = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EmptyWeight = table.Column<float>(type: "real", nullable: false),
+                    FuelTank = table.Column<int>(type: "int", nullable: true),
+                    FuelConsumption = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CylinderVolume = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CylinderNumber = table.Column<int>(type: "int", nullable: false),
+                    ValveNumber = table.Column<int>(type: "int", nullable: false),
+                    CaseType = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EnginePower = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Color = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TypeOfTransfer = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    ExpertizId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Advert", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Advert_CarModels_CarModelId",
+                        column: x => x.CarModelId,
+                        principalTable: "CarModels",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Advert_Equipment_EquipmentId",
+                        column: x => x.EquipmentId,
+                        principalTable: "Equipment",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Advert_Expertiz_ExpertizId",
+                        column: x => x.ExpertizId,
+                        principalTable: "Expertiz",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Galleries",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Image = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AdvertId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Galleries", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Galleries_Advert_AdvertId",
+                        column: x => x.AdvertId,
+                        principalTable: "Advert",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Advert_CarModelId",
+                table: "Advert",
+                column: "CarModelId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Advert_EquipmentId",
+                table: "Advert",
+                column: "EquipmentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Advert_ExpertizId",
+                table: "Advert",
+                column: "ExpertizId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CarModels_CarBrandId",
+                table: "CarModels",
+                column: "CarBrandId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CarModels_CategoryId",
+                table: "CarModels",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Equipment_CarModelId",
+                table: "Equipment",
+                column: "CarModelId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Galleries_AdvertId",
+                table: "Galleries",
+                column: "AdvertId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Admin");
+
+            migrationBuilder.DropTable(
+                name: "Galleries");
+
+            migrationBuilder.DropTable(
+                name: "Advert");
+
+            migrationBuilder.DropTable(
+                name: "Equipment");
+
+            migrationBuilder.DropTable(
+                name: "Expertiz");
+
+            migrationBuilder.DropTable(
+                name: "CarModels");
+
+            migrationBuilder.DropTable(
+                name: "CarBrands");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+        }
+    }
+}
